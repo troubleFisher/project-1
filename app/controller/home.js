4fbcd0e1d553ff47de06f9ffda833d3dac2878c0
tangyu <tangyu@medlinker.com>
2021-01-14 14:26:56 +0800

    chore(home.contaroller): 测试修改

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,6 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+    console.log('1');
     ctx.body = 'hi, egg';
   }
 }
